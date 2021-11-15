82d8c4afcb7a3f3d4c28853fd9295fa426e2e147
Elliot Thompson <elliotthompson101@gmail.com>
2021-11-15 11:00:58 +0000

    created Dao with insert, update, clear & retrieve queries

--- app/src/main/java/com/github/kovah101/chargemycar/savedDatabase/ChargeDatabaseDAO.kt
+++ app/src/main/java/com/github/kovah101/chargemycar/savedDatabase/ChargeDatabaseDAO.kt
@@ -1,4 +1,39 @@
 package com.github.kovah101.chargemycar.savedDatabase
 
-class ChargeDatabaseDAO {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+// interface to add/remove charge points from a saved list of favourites
+
+@Dao
+interface ChargeDatabaseDAO {
+    // insert 1 specific charge point
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(point: ChargePoint)
+
+    // insert a list of charge points
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(chargePoints: List<ChargePoint>)
+
+    // updates the data for a charge point
+    @Update
+    fun update(point: ChargePoint)
+
+    // get all saved charge points
+    @Query("SELECT * FROM saved_charge_points_table ORDER BY chargePointId DESC")
+    fun getAllPoints(): LiveData<List<ChargePoint>>
+
+    // get a specific charge point
+    // might have to change to key: PrimaryKey
+    @Query("SELECT * FROM saved_charge_points_table WHERE chargePointId = :key")
+    fun getPoint(key: Long): ChargePoint?
+
+    // delete specific charge point
+    @Query("DELETE FROM saved_charge_points_table WHERE chargePointId = :key")
+    fun removePoint(key: Long)
+
+    // clear database
+    @Query("DELETE FROM saved_charge_points_table")
+    fun clear()
+
 }
