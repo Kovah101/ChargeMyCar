cebba6faf93f9cc7d78c6ab78ab30ca516528a09
Elliot Thompson <elliotthompson101@gmail.com>
2021-12-04 09:33:59 +0000

    Completed formatChargePoints dummy data function

--- app/src/main/java/com/github/kovah101/chargemycar/Util.kt
+++ app/src/main/java/com/github/kovah101/chargemycar/Util.kt
@@ -1,7 +1,10 @@
 package com.github.kovah101.chargemycar
 
 import android.content.res.Resources
+import android.os.Build
+import android.text.Html
 import android.text.Spanned
+import androidx.core.text.HtmlCompat
 import com.github.kovah101.chargemycar.savedDatabase.ChargePoint
 
 /**
@@ -9,6 +12,30 @@ import com.github.kovah101.chargemycar.savedDatabase.ChargePoint
  */
 
 
-fun formatChargePoints(chargePoints : List<ChargePoint>, resources : Resources) {
+fun formatChargePoints(chargePoints : List<ChargePoint>, resources : Resources): Spanned {
+    val sb = StringBuilder()
+    sb.apply {
+        append(resources.getString(R.string.text_title))
+        chargePoints.forEach{
+            append("<br>")
+            append(resources.getString(R.string.latitude))
+            append("\t${it.latitude}<br>")
+            append(resources.getString(R.string.longitude))
+            append("\t${it.longitude}<br>")
+            append(resources.getString(R.string.postcode))
+            append("\t${it.postcode}<br>")
+            append(resources.getString(R.string.connector_type))
+            append("\t${it.connectorType}<br>")
+            append(resources.getString(R.string.charge_point_status))
+            append("\t${it.chargePointStatus}<br>")
+            append(resources.getString(R.string.location_type))
+            append("\t${it.locationType}<br>")
+        }
+    }
 
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        return Html.fromHtml(sb.toString(), Html.FROM_HTML_MODE_LEGACY)
+    } else {
+        return HtmlCompat.fromHtml(sb.toString(), HtmlCompat.FROM_HTML_MODE_LEGACY)
+    }
 }
