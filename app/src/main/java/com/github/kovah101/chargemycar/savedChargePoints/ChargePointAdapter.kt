caccd956e46c9d37973e95d88be926913231fb44
Elliot Thompson <elliotthompson101@gmail.com>
2021-12-09 10:31:14 +0000

    Implemented new ViewHolder basics

--- app/src/main/java/com/github/kovah101/chargemycar/savedChargePoints/ChargePointAdapter.kt
+++ app/src/main/java/com/github/kovah101/chargemycar/savedChargePoints/ChargePointAdapter.kt
@@ -1,14 +1,20 @@
 package com.github.kovah101.chargemycar.savedChargePoints
 
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.GradientDrawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.compose.ui.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import com.github.kovah101.chargemycar.R
+import com.github.kovah101.chargemycar.R.*
 import com.github.kovah101.chargemycar.TextItemViewHolder
 import com.github.kovah101.chargemycar.savedDatabase.ChargePoint
 
-class ChargePointAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+class ChargePointAdapter: RecyclerView.Adapter<ChargePointAdapter.ViewHolder>() {
 
     var data = listOf<ChargePoint>()
         set(value) {
@@ -20,18 +26,33 @@ class ChargePointAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
 
 
     // test with postcode data
-    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.locationType
+        val res = holder.itemView.context.resources
+        holder.postcode.text = item.postcode
+        //val trueDistance = HaversineDistance(dummyUserLat, dummyUserLong, item.latitude, item.longitude)
+        holder.distance.text = item.locationType
+        //holder.distance.background.colorFilter = DistanceColor(trueDistance)
+        holder.connectorType.text = item.connectorType
+        holder.status.text = item.chargePointStatus.toString()
+        // change status text color
+
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater
-            .inflate(R.layout.text_item_view, parent, false) as TextView
+            .inflate(layout.charge_point_list_item, parent, false)
 
-        return TextItemViewHolder(view)
+        return ViewHolder(view)
+    }
+
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val postcode : TextView = itemView.findViewById(id.postcode)
+        val distance : TextView = itemView.findViewById(id.distance)
+        val connectorType : TextView = itemView.findViewById(id.connectorType)
+        val status : TextView = itemView.findViewById(id.status)
     }
 }
 
