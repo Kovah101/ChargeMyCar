78551683757650ef8d8273dcd2aa4603e071b256
Elliot Thompson <elliotthompson101@gmail.com>
2022-02-16 09:08:56 +0000

    fixing saved map initialise bug

--- app/src/main/java/com/github/kovah101/chargemycar/savedChargePoints/SavedMapFragment.kt
+++ app/src/main/java/com/github/kovah101/chargemycar/savedChargePoints/SavedMapFragment.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.github.kovah101.chargemycar.R
@@ -15,6 +16,7 @@ import com.github.kovah101.chargemycar.viewModel.ChargePointViewModel
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import timber.log.Timber
 
 /**
  * Saved Charge Points displayed on a map from database.
@@ -48,10 +50,11 @@ class SavedMapFragment : Fragment(), OnMapReadyCallback {
         val mapFragment = childFragmentManager.findFragmentById(R.id.savedMap) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-
-
-
         binding.lifecycleOwner = this
+
+        savedPointsViewModel.chargePoints.observe(viewLifecycleOwner, Observer {
+            Timber.d(" 1 - map points observed")
+        })
 
         return binding.root
     }
@@ -68,31 +71,57 @@ class SavedMapFragment : Fragment(), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        // center map on London TODO:  center on average location add more options and animations
+        // center map on London TODO:  fix first load bug & add animations, try with database??
         var centerLat = 0.0
         var centerLong = 0.0
         // add markers for each saved charge point
         // TODO customise icon + add onClick behaviour
         val savedPointsViewModel: ChargePointViewModel by activityViewModels()
-        val savedChargePoints = savedPointsViewModel.chargePoints.value
-        savedChargePoints?.forEach { chargePoint ->
-            centerLat += chargePoint.latitude.toDouble()
-            centerLong += chargePoint.longitude.toDouble()
-            val cpLocation =
-                LatLng(chargePoint.latitude.toDouble(), chargePoint.longitude.toDouble())
-            val cpName = chargePoint.postcode
-            googleMap.addMarker(
-                MarkerOptions().position(cpLocation)
-                    .title(cpName)
-            )
-        }
-        if (savedChargePoints != null) {
-            centerLat /= savedChargePoints.size
-            centerLong /= savedChargePoints.size
-        } else {
+        savedPointsViewModel.chargePoints.observe(viewLifecycleOwner, Observer {
+            it.forEach { chargePoint ->
+                centerLat += chargePoint.latitude.toDouble()
+                centerLong += chargePoint.longitude.toDouble()
+                val cpLocation =
+                    LatLng(chargePoint.latitude.toDouble(), chargePoint.longitude.toDouble())
+                val cpName = chargePoint.postcode
+                googleMap.addMarker(
+                    MarkerOptions().position(cpLocation)
+                        .title(cpName)
+                )
+            }
+            centerLat /= it.size
+            centerLong /= it.size
+            Timber.d("2A - map points - center on: $centerLat, $centerLong")
+//            val centerLondon = LatLng(centerLat, centerLong)
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLondon, 14f))
+        })
+        if (centerLat == 0.0){
+            Timber.d("2B - map points - charge points == null, $centerLat + $centerLong")
             centerLat = 51.509865
             centerLong = -0.118092
+
         }
+//        val savedChargePoints = savedPointsViewModel.chargePoints.value
+//        savedChargePoints?.forEach { chargePoint ->
+//            centerLat += chargePoint.latitude.toDouble()
+//            centerLong += chargePoint.longitude.toDouble()
+//            val cpLocation =
+//                LatLng(chargePoint.latitude.toDouble(), chargePoint.longitude.toDouble())
+//            val cpName = chargePoint.postcode
+//            googleMap.addMarker(
+//                MarkerOptions().position(cpLocation)
+//                    .title(cpName)
+//            )
+//        }
+//        if (savedChargePoints != null) {
+//            centerLat /= savedChargePoints.size
+//            centerLong /= savedChargePoints.size
+//            Timber.d("Will center map on: $centerLat, $centerLong")
+//        } else {
+//            centerLat = 51.509865
+//            centerLong = -0.118092
+//            Timber.d("Will center map on: $centerLat, $centerLong")
+//        }
         val centerLondon = LatLng(centerLat, centerLong)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(centerLondon, 14f))
     }
